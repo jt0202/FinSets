05a7ca9dc929f536b936f3302653790b84305d43
Johannes <johannes.tantow@gmail.com>
2023-05-29 23:59:17 +0200

    mathlib support + size results for Kuratowski sets

--- src/Kuratowski.lean
+++ src/Kuratowski.lean
@@ -3,6 +3,7 @@ open decidable
 open nat
 open list
 
+
 universe u
 
 namespace Kuratowksi
@@ -67,13 +68,34 @@ def fset_under_map (A: Type u) (B: Type u): Kuratowski A -> (A -> B) -> Kuratows
 def kuratowski_member {A:Type u} [decidable_eq A] : A -> Kuratowski A -> bool
 | x Kuratowski.empty := false
 | x {y} := x=y
-| x (y ∪ z) := (kuratowski_member x z) ∨ (kuratowski_member x y)
+| x (y ∪ z) := (kuratowski_member x z)= tt ∨ (kuratowski_member x y) = tt
 
 def kuratowski_member_prop {A:Type u} [decidable_eq A] : A -> Kuratowski A -> Prop
 | x Kuratowski.empty := ff
 | x {y} := x=y
 | x (y ∪ z) := (kuratowski_member_prop x z) ∨ (kuratowski_member_prop x y)
 
+lemma kuratowski_member_iff_kuratowski_member_prop {A:Type u} [decidable_eq A] (a:A) (X: Kuratowski A): (kuratowski_member a X = tt) ↔ kuratowski_member_prop a X :=
+begin
+  induction X with a' X1 X2 h_X1 h_X2,
+  simp [kuratowski_member, kuratowski_member_prop],
+  by_cases (a=a'),
+  rw h,
+  simp [kuratowski_member, kuratowski_member_prop],
+  simp [kuratowski_member, kuratowski_member_prop, h],
+  simp [kuratowski_member, kuratowski_member_prop],
+  rw ← h_X1,
+  rw ← h_X2,
+end
+
+def boolean_negation: bool -> bool
+| tt := ff
+| ff := tt
+
+lemma kuratowski_member_false_iff_neg_kuratowski_member_prop {A:Type u} [decidable_eq A] (a:A) (X: Kuratowski A): ¬ (kuratowski_member a X) = tt ↔ ¬ kuratowski_member_prop a X :=
+begin
+  rw ← kuratowski_member_iff_kuratowski_member_prop,
+end
 
 section extensionality
 variables {A: Type u} [decidable_eq A]
@@ -204,7 +226,7 @@ begin
   rw h_right,
 end
 
-theorem extensionaliy (X Y: Kuratowski A): X = Y ↔ (∀ (a:A), kuratowski_member_prop a X = kuratowski_member_prop a Y):=
+theorem extensionality (X Y: Kuratowski A): X = Y ↔ (∀ (a:A), kuratowski_member_prop a X = kuratowski_member_prop a Y):=
 begin
   split,
   rw eq_subset2,
@@ -287,13 +309,21 @@ begin
 end
 
 
-theorem empty_set_has_size_0 {A:Type u} [∀ (a: A) (L: list A), decidable (a ∈ L)] (x: Kuratowski A) (h: x = Kuratowski.empty): (size (x) = 0) :=
+lemma empty_set_has_size_0 {A:Type u} [∀ (a: A) (L: list A), decidable (a ∈ L)] (x: Kuratowski A) (h: x = Kuratowski.empty): (size (x) = 0) :=
 begin
   unfold size,
   rw h,
   unfold kuratowski_to_list,
   unfold set_size_of_list,
 end
+
+lemma singleton_has_size_1 {A:Type u} [∀ (a: A) (L: list A), decidable (a ∈ L)] (a:A): (size ({a}) = 1) :=
+begin
+  unfold size,
+  unfold kuratowski_to_list,
+  simp [set_size_of_list],
+end
+
 
 theorem union_with_member_does_not_increase_size {A:Type u} [decidable_eq A] [∀ (a: A) (L: list A), decidable (a ∈ L)] (x: Kuratowski A) (a: A) (h: (kuratowski_member_prop a x)): (size (x) = size ( {a} ∪ x)) :=
 begin
@@ -306,16 +336,198 @@ end
 
 def comprehension{A:Type u}: (A -> bool) -> Kuratowski A -> Kuratowski A
 | ϕ Kuratowski.empty := Kuratowski.empty
-| ϕ {a} := if ϕ a then {a} else Kuratowski.empty
+| ϕ {a} := if (ϕ a = tt) then {a} else Kuratowski.empty
 | ϕ (x ∪ y) := comprehension ϕ x ∪ comprehension ϕ y
+
+lemma and_or_distr (P Q R: Prop): P ∧ Q ∨ P ∧ R ↔ P ∧ (Q ∨ R) :=
+begin
+  by_cases P,
+  simp [h],
+  simp [h],
+end
+
+lemma comprehension_semantics {A:Type u} [decidable_eq A](p: A -> bool) (X : Kuratowski A) (a: A): kuratowski_member_prop a (comprehension p X) ↔ (p a = tt) ∧ kuratowski_member_prop a X :=
+begin
+  induction X with a' x1 x2 h_x1 h_x2,
+  simp [comprehension, kuratowski_member_prop],
+  unfold comprehension,
+  by_cases (p a' = tt),
+  simp[h, kuratowski_member_prop],
+  by_cases h': (a = a'),
+  simp [h'],
+  exact h,
+  simp [h'],
+  simp [h],
+  by_cases h' : (a = a'),
+  simp [h', h, kuratowski_member_prop],
+  simp [kuratowski_member_prop, h'],
+
+  simp [comprehension, kuratowski_member_prop],
+  rw h_x1,
+  rw h_x2,
+  rw and_or_distr,
+end
 
 def kuratowski_intersection {A:Type u} [decidable_eq A] (x y:Kuratowski A) : Kuratowski A:= comprehension (λ (a:A),kuratowski_member a y) x
 
 notation (name := kuratowski_intersection) x ∩ y := kuratowski_intersection x y
 
+
+lemma in_intersection_iff_in_both {A:Type u} [decidable_eq A] (X Y: Kuratowski A) (a:A): kuratowski_member_prop a (X ∩ Y) = (kuratowski_member_prop a X ∧ kuratowski_member_prop a Y):=
+begin
+  unfold kuratowski_intersection,
+  rw comprehension_semantics,
+  rw kuratowski_member_iff_kuratowski_member_prop,
+  rw and.comm,
+end
+
 def disjoint {A: Type u} [decidable_eq A] (X Y: Kuratowski A):= (kuratowski_intersection X Y) = Kuratowski.empty
 
-def kuratowski_difference {A:Type u} [decidable_eq A] (x y:Kuratowski A): Kuratowski A := comprehension (λ (a:A), ¬ kuratowski_member a y) x
+
+def kuratowski_difference {A:Type u} [decidable_eq A] (x y:Kuratowski A): Kuratowski A := comprehension (λ (a:A), ¬ ( kuratowski_member a y = tt))  x
+
+notation (name := kuratowski_difference) x \ y := kuratowski_difference x y
+
+lemma test (a: bool): to_bool(¬ a = tt) =tt ↔ ¬ a = tt :=
+begin
+  cases a,
+  simp,
+  simp,
+end
+
+lemma in_difference_if_only_in_first {A:Type u} [decidable_eq A] (X Y: Kuratowski A) (a:A): kuratowski_member_prop a (X \ Y) = (kuratowski_member_prop a X ∧ ¬  kuratowski_member_prop a Y) :=
+begin
+  dunfold kuratowski_difference,
+  rw comprehension_semantics,
+  rw test,
+  rw kuratowski_member_false_iff_neg_kuratowski_member_prop,
+  rw and.comm,
+end
+
+lemma intersection_and_difference_are_disjoint {A:Type u} [decidable_eq A] (X Y: Kuratowski A) : disjoint (X ∩ Y) (X \ Y) :=
+begin
+  unfold disjoint,
+  rw extensionality,
+  intro a,
+  rw in_intersection_iff_in_both,
+  rw in_intersection_iff_in_both,
+  rw in_difference_if_only_in_first,
+  by_cases h: (kuratowski_member_prop a Y),
+  simp [h, kuratowski_member_prop],
+  simp [h, kuratowski_member_prop],
+end
+
+
+lemma disjoint_of_union {A: Type u} [decidable_eq A] (X1 X2 X Y: Kuratowski A) (d: disjoint X Y) (h: X= X1 ∪ X2): disjoint X1 Y :=
+begin
+  rw h at d,
+  rw disjoint at d,
+  rw extensionality at d,
+  rw disjoint,
+  rw extensionality,
+  intro a',
+  have d_a': kuratowski_member_prop a' ((X1∪X2)∩Y) = kuratowski_member_prop a' Kuratowski.empty,
+  apply d,
+  rw ← d_a',
+  rw in_intersection_iff_in_both,
+  rw in_intersection_iff_in_both,
+  simp [kuratowski_member_prop],
+  by_cases aY: kuratowski_member_prop a' Y,
+  simp [aY],
+  by_cases aX2: kuratowski_member_prop a' X2,
+  exfalso,
+  rw in_intersection_iff_in_both at d_a',
+  simp [kuratowski_member_prop, aY, aX2] at d_a',
+  rw ← d_a',
+  exact true.intro,
+  simp [aX2],
+  simp [aY],
+end
+
+lemma set_as_difference_and_intersection {A: Type u} [decidable_eq A] (X Y:Kuratowski A): X = (X ∩ Y) ∪ (X \ Y):=
+begin
+  rw extensionality,
+  intro a,
+  unfold kuratowski_member_prop,
+  rw in_difference_if_only_in_first,
+  rw in_intersection_iff_in_both,
+  by_cases (kuratowski_member_prop a Y),
+  simp [h],
+  simp [h],
+end
+
+lemma set_and_difference_are_union {A: Type u} [decidable_eq A] (X Y: Kuratowski A): X ∪ (Y \ X) = (X ∪ Y) :=
+begin
+  rw extensionality,
+  intro a,
+  simp [kuratowski_member_prop],
+  rw in_difference_if_only_in_first,
+  by_cases aX: kuratowski_member_prop a X,
+  simp [aX],
+  simp [aX],
+end
+
+lemma set_and_difference_disjoint {A: Type u} [decidable_eq A] (X Y: Kuratowski A): disjoint X (Y \ X) :=
+begin
+  unfold disjoint,
+  rw extensionality,
+  intro a,
+  rw in_intersection_iff_in_both,
+  rw in_difference_if_only_in_first,
+  by_cases aX: kuratowski_member_prop a X,
+  simp [aX,kuratowski_member_prop],
+  simp [aX,kuratowski_member_prop],
+
+end
+
+lemma length_disjoint {A:Type u} [decidable_eq A] [∀ (a: A) (L: list A), decidable (a ∈ L)] (X Y: Kuratowski A) (h: disjoint X Y): size(X ∪ Y) = size(X) + size(Y) :=
+begin
+  induction X with a X1 X2 h_X1 h_X2,
+  simp [empty_union, empty_set_has_size_0],
+  rw nat.zero_add,
+  rw singleton_has_size_1,
+  unfold size,
+  unfold kuratowski_to_list,
+  simp [set_size_of_list],
+  have aY: ¬ a ∈ kuratowski_to_list Y,
+  rw ←  kuratowski_to_list_preserves_membership,
+  rw disjoint at h,
+  rw extensionality at h,
+  have h': kuratowski_member_prop a   ({a} ∩ Y) = kuratowski_member_prop a Kuratowski.empty,
+  apply h,
+  rw in_intersection_iff_in_both at h',
+  simp [kuratowski_member_prop] at h',
+  rw h',
+  simp,
+  simp [aY],
+  rw nat.add_comm,
+
+
+
+
+
+end
+
+
+
+theorem inclusion_exclusion  {A:Type u} [decidable_eq A] [∀ (a: A) (L: list A), decidable (a ∈ L)] (X Y: Kuratowski A): size(X) + size (Y) = size (X ∪ Y) + size (X ∩ Y) :=
+begin
+  have size_x: (size(X) = size( X ∩ Y ∪ X \ Y)),
+  conv
+  begin
+    to_lhs,
+    rw set_as_difference_and_intersection X Y,
+    skip,
+  end,
+  rw size_x,
+  rw length_disjoint (X ∩ Y) (X \ Y) (intersection_and_difference_are_disjoint X Y) ,
+  rw nat.add_assoc,
+  rw nat.add_comm (size(X \ Y)) _,
+  rw ← length_disjoint Y (X \ Y) (set_and_difference_disjoint Y X),
+  rw set_and_difference_are_union,
+  rw nat.add_comm,
+  rw union_comm Y X,
+end
 
 end Length
 
---
+++ src/RedBlackTree.lean
@@ -0,0 +1,99 @@
+-- set_option trace.eqn_compiler.elim_match true
+
+inductive Color
+| red: Color 
+| black: Color
+
+inductive RedBlackTree (A : Type)
+| empty: RedBlackTree 
+| node : Color -> A -> RedBlackTree -> RedBlackTree -> RedBlackTree
+
+def member {A: Type} [decidable_eq A] [preorder A] [∀ (a b: A), decidable( a < b)]: A -> RedBlackTree A -> Prop
+| x RedBlackTree.empty := false 
+| x (RedBlackTree.node _ a tl tr) := 
+  if (x = a) 
+    then true 
+    else  member x tl ∨  member x tr
+
+def unbalanced_insert {A: Type} [preorder A] [decidable_eq A] [∀ (a b: A), decidable( a < b)]: A -> RedBlackTree A -> RedBlackTree A
+| x RedBlackTree.empty := (RedBlackTree.node Color.black x RedBlackTree.empty RedBlackTree.empty)
+| x (RedBlackTree.node c a tl tr) := 
+  if (x = a)
+    then (RedBlackTree.node c a tl tr)
+  else if x < a 
+    then RedBlackTree.node c a (unbalanced_insert x tl) tr
+  else
+    RedBlackTree.node c a tl (unbalanced_insert x tr)
+
+def size {A:Type} :RedBlackTree A ->  ℕ
+| RedBlackTree.empty := 0
+| (RedBlackTree.node _ a tl tr) := 1 + size tl + size tr
+
+def forall_keys {A: Type} [decidable_eq A] [preorder A][∀ (a b: A), decidable( a < b)] (p: A -> A -> Prop) (a : A) (t: RedBlackTree A): Prop :=
+  ∀ a', (member a' t) -> p a a'
+
+def ordered {A: Type} [decidable_eq A] [preorder A] [∀ (a b: A), decidable( a < b)]: RedBlackTree A -> Prop
+| RedBlackTree.empty := tt
+| (RedBlackTree.node _ x tl tr) := ordered tl ∧ ordered tr ∧ (forall_keys (>) x tl) ∧ (forall_keys (<) x tr)
+
+lemma lt_implies_neq {A:Type} [decidable_eq A] [preorder A] {a x: A}: (a < x) -> ¬ (a = x) :=
+begin
+  intro h,
+  by_contradiction h1,
+  rw h1 at h,
+  apply lt_irrefl x,
+  exact h,
+end
+
+lemma lt_all_eq_no_member {A:Type} [decidable_eq A] [preorder A] [∀ (a b: A), decidable( a < b)] (T: RedBlackTree A) (o: ordered(T)) (a: A) (p: ∀ (x:A), member x T -> x > a): ¬ (member a T) :=
+begin
+  induction T with c y tl tr h_tl h_tr,
+  unfold member,
+  simp,
+  unfold member,
+  simp,
+  apply not_or_distrib,
+end
+
+lemma ordered_less_than_eq_in_left_tree {A:Type} [decidable_eq A] [preorder A] [∀ (a b: A), decidable( a < b)] {a: A} {tl tr: RedBlackTree A} {c:Color}(T: RedBlackTree A) (T_node: T = (RedBlackTree.node c a tl tr) )(o:ordered(T)) (x: A) (h: member x T) (lt: x < a) : member x tl  :=
+begin
+  cases T,
+  exfalso,
+  unfold member at h,
+  apply h,
+
+  have neq: ¬(x=a),
+  apply lt_implies_neq,
+  exact lt,
+  rw T_node at h,
+  rw T_node at o,
+  dunfold member at h,
+  simp [neq] at h,
+end
+
+theorem cant_unbalanced_insert_twice {A:Type} [preorder A] [decidable_eq A] [∀ (a b: A), decidable( a < b)] (a:A) (T: RedBlackTree A) (h: member a T) : T = unbalanced_insert a T :=
+begin
+  induction T with c x tl tr h_tl h_tr,
+  exfalso,
+  unfold member at h,
+  apply h,
+  by_cases h1: (a= x),
+  rw h1,
+  dunfold unbalanced_insert, -- unfold doesn't work here
+  simp [h1],
+  by_cases h2: (a < x),  
+  dunfold unbalanced_insert,
+  simp [h1, h2],
+  apply h_tl,
+  admit,
+  dunfold unbalanced_insert,
+  simp [h1, h2],
+  apply h_tr,
+  admit,
+end
+
+
+def union {A:Type} [preorder A] [decidable_eq A] [∀ (a b: A), decidable( a < b)]: RedBlackTree A -> RedBlackTree A -> RedBlackTree A
+| B RedBlackTree.empty := B
+| B (RedBlackTree.node c x tl tr) := union ( union (unbalanced_insert x B) tl) tr
+
